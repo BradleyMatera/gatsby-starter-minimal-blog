05744330f25afd63ebcf16a8150ee84618465772
MateraBradley-FS-1 <bfmatera@student.fullsail.edu>
2026-03-06 18:52:09 -0600

    fix: align SSR style imports with active CSS bundle

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,9 +2,9 @@ import React from "react";
 import { withPrefix } from "gatsby";
 
 // Keep SSR style order aligned with gatsby-browser to avoid cascade mismatches.
-import "./src/styles/cyberpunk-theme.css";
-import "./src/styles/cyberpunk-nav.css";
 import "./src/styles/global.css";
+import "./src/styles/site-chrome.css";
+import "./src/styles/utilities.css";
 import "./src/styles/vertical-nav.css";
 import "./src/styles/media.css";
 
